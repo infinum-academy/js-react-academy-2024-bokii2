ac3c27aa6a63ba6d97530499f30af161fa9b1aaa
Bojan Popovski <bojpopovski2002@gmail.com>
2024-07-07 22:15:21 +0200

    Some improvements on ShowDetails

--- homework-2/src/app/page.tsx
+++ homework-2/src/app/page.tsx
@@ -1,18 +1,23 @@
-import Image from "next/image";
+import { Heading } from "@chakra-ui/react";
 import styles from "./page.module.css";
 import { ShowDetails } from "@/components/features/shows/ShowDetails/ShowDetails";
 
 const someShow = {
   title: "Game Of Thrones",
-  description: "Some description",
+  description: `Game of Thrones is roughly based on the storylines of the A Song of Ice and Fire book series by George R. R. Martin,
+                set in the fictional Seven Kingdoms of Westeros and the continent of Essos. The series follows several simultaneous
+                plot lines. The first story arc follows a war of succession among competing claimants for control of the Iron Throne 
+                of the Seven Kingdoms, with other noble families fighting for independence from the throne. The second concerns the 
+                exiled scion &apos; actions to reclaim the throne; the third chronicles the threat of the impending winter, as well as the 
+                legendary creatures and fierce peoples of the North.`,
   averageRating: 0,
-  imageUrl: "https://media.glamour.com/photos/5fa2e9c958f1ae62b4f610e8/master/w_2560%2Cc_limit/tv-2.jpg"
+  imageUrl: "game-of-thrones.jpg"
 }
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>TV Shows App</h1>
+      <Heading as='h3' size='lg'>TV Shows App</Heading>
       <ShowDetails show={someShow} />
     </main>
   );
--- homework-2/src/components/features/shows/ShowDetails/ShowDetails.tsx
+++ homework-2/src/components/features/shows/ShowDetails/ShowDetails.tsx
@@ -1,4 +1,4 @@
-import { Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Container, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 interface IShow {
     show: {
@@ -11,11 +11,13 @@ interface IShow {
 
 export const ShowDetails = (props: IShow) => {
     return (
-        <div>
-            <Image alt="got" src={props.show.imageUrl} />
-            <Heading as="h2">{props.show.title}</Heading>
-            <p>{props.show.description}</p>
-            <Heading as="h3">{props.show.averageRating != 0 ? props.show.averageRating : "No rating yet"}</Heading>
-        </div>
+        <Card width='920px' direction='column' borderRadius='10px' backgroundColor='white' color='#5935bc'>
+            <Image alt="got" src={props.show.imageUrl} borderTopRadius='inherit' />
+            <CardBody padding='30px' marginTop='20px' gap='8px'>
+                <Heading as='h2' size='2xl'>{props.show.title}</Heading>
+                <Text fontSize='sm' margin='15px 0'>{props.show.description}</Text>
+                <Heading as='h5' size='sm'>{props.show.averageRating != 0 ? props.show.averageRating : "No rating yet"}</Heading>
+            </CardBody>
+        </Card>
     );
 };
