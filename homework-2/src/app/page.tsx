1853140764a0987dee2d7c7bc4653e09f4f261f1
Bojan Popovski <bojpopovski2002@gmail.com>
2024-07-13 22:48:46 +0200

    Added file with tests for ShowCard

--- homework-2/src/app/page.tsx
+++ homework-2/src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  router.push('/all-shows');
+  useEffect(() => {
+    router.push('/all-shows');
+  }, []);
 
   return (
     <main className={styles.main}>
