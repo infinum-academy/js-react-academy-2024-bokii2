332844f6fb2d357abb7056ee4c0f7e382198b474
Bojan Popovski <bojpopovski2002@gmail.com>
2024-07-23 12:02:57 +0200

    NotFound page added

---
+++ homework-2/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function NotFound() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/all-shows');
+    }, [router]);
+
+    return (
+      <></>
+    )
+}
--- homework-2/src/app/providers.tsx
+++ homework-2/src/app/providers.tsx
@@ -2,30 +2,13 @@
 'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { SWRConfig } from 'swr'
-import AllShows from './(shows)/all-shows/page'
-import AllShowsDetails from './(shows)/all-shows/[id]/page'
-import Login from './login/page'
-import Register from './register/page'
-import TopRated from './(shows)/top-rated/page'
-import Home from './page'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SWRConfig>
       <ChakraProvider>
         {children}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/all-shows" element={<AllShows />} />
-            <Route path="/all-shows/:id" element={<AllShowsDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/top-rated" element={<TopRated />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
       </ChakraProvider>
     </SWRConfig>
   )
